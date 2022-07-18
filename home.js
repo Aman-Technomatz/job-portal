0db00b6b045f0c1e6dc9201b22fcf9f38051e3d6
aman <atomar@technomatz.com>
2022-07-18 17:38:05 +0530

    final commit

--- home.js
+++ home.js
@@ -1,31 +1,31 @@
-function fetchData(){
-    fetch("https://www.arbeitnow.com/api/job-board-api")
-    .then(response => {
-            if(!response.ok){
-                throw Error("ERROR");
-            }
-            return response.json();
+function fetchData() {
+  fetch("https://www.arbeitnow.com/api/job-board-api")
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
-    .then(data => {
-        console.log(data.data);
-        const html = data.data 
-        .map(user => {
-            return`
+    .then((data) => {
+      console.log(data.data);
+      const html = data.data
+        .map((user) => {
+          return `
             <div class="card" style="float:left;">
-            <p>Job: ${user.title}</p>
-    <p>Job Type: ${user.job_types}</p>
-  <p>Company_name: ${user.company_name}</p>
-  <p>${user.tags}</p>
-  <p><button>Search</button></p>
-</div>    
+            <p><b>Job:</b> ${user.title}</p>
+            <p><b>Job Type:</b> ${user.job_types}</p>
+            <p><b>Company name:</b> ${user.company_name}</p>
+            <p><b>Job Field:</b>${user.tags}</p>
+            <p><button>Select</button></p>
+        </div>    
             `;
         })
         .join("");
-        console.log(html);
-        document.querySelector("#app").insertAdjacentHTML("afterbegin",html);
+      console.log(html);
+      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
     })
-    .cache(error => {
-        console.log(error);
-    })
+    .cache((error) => {
+      console.log(error);
+    });
 }
 fetchData();
--- signin.js
+++ signin.js
@@ -1,27 +1,57 @@
+function savedata() {
+  let name, mob, email, pass;
 
-function savedata(){
-	let name,mob,email,pass;
+  name = document.getElementById("name").value;
+  mob = document.getElementById("mob").value;
+  email = document.getElementById("email").value;
+  pass = document.getElementById("pass").value;
+  let user_record = new Array();
+  user_record = JSON.parse(localStorage.getItem("user"))
+    ? JSON.parse(localStorage.getItem("user"))
+    : [];
+  if (
+    user_record.some((v) => {
+      return v.email == email;
+    })
+  ) {
+    alert("duplicate data");
+  } else {
+    user_record.push({
+      name: name,
+      mob: mob,
+      email: email,
+      pass: pass,
+    });
+    localStorage.setItem("user", JSON.stringify(user_record));
+    window.location.href = "signin.html";
+  }
+}
 
-	name = document.getElementById("name").value;
-	mob = document.getElementById("mob").value;
-	email = document.getElementById("email").value;
-	pass = document.getElementById("pass").value;
-	let user_record = new Array();
-	user_record =JSON.parse(localStorage.getItem("user"))?JSON.parse(localStorage.getItem("user")):[] 
-	if(user_record.some((v)=>{return v.email==email}))
-	{
-		alert("duplicate data");
-	}
-	else{
-user_record.push({
-		"name":name,
-		"mob":mob,
-		"email":email,
-		"pass":pass
-	})
-	localStorage.setItem("user",JSON.stringify(user_record));
-	
-	}
- }
+function loginData() {
+  let email, pass;
+  var checkBox = document.getElementById("myCheck");
+  email = document.getElementById("email").value;
 
- 
+  pass = document.getElementById("pass").value;
+
+  let user_records = new Array();
+  user_records = JSON.parse(localStorage.getItem("user"))
+    ? JSON.parse(localStorage.getItem("user"))
+    : [];
+  if (
+    checkBox.checked == true &&
+    user_records.some((v) => {
+      return v.email == email && v.pass == pass;
+    })
+  ) {
+    alert("Login Pass");
+    let current_user = user_records.filter((v) => {
+      return v.email == email && v.pass == pass;
+    })[0];
+    localStorage.setItem("name", current_user.name);
+    localStorage.setItem("email", current_user.email);
+    window.location.href = "home.html";
+  } else {
+    alert("Login Fail");
+  }
+}
